99ca7b5d3ba430738d565a4048eea41d7b92996e
vieirabarros <vieirabarros.filipe@gmail.com>
2024-11-19 16:40:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const tarefa=document.querySelector("#tarefa");
+const btn=document.querySelector("#btn");
+const lista=document.querySelector("#lista")
+
+
+
+btn.addEventListener("click",function(){
+    if (tarefa.value==""){
+        alert("Digite uma tarefa válida")
+    }
+    else {
+        lista.innerHTML+=
+            `
+            <li>
+                <i class="fas fa-check-circle check"></i>
+                <span>${tarefa.value}</span>
+                <i class="fas fa-trash-can close"></i>
+            </li>
+            `
+    }
+    tarefa.value=""; //Apagar do campo após incluir
+
+    const close=document.querySelectorAll(".close");    
+    for (let i=0;i<close.length;i++){
+            close[i].addEventListener("click", function(){
+                close[i].parentElement.remove();
+            })
+    }
+
+    lista.addEventListener("click", function(e){
+        e.target.parentElement.querySelector(".check").style.color="#349223";
+        e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
+    })
+})

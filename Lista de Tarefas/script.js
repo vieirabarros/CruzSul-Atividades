9018e8eabaef6ac258fd20aff80603c54e5e2007
vieirabarros <vieirabarros.filipe@gmail.com>
2024-11-20 15:27:16 -0300

    message

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,3 +1,49 @@
+$(document).ready(
+    function(){
+        $("#btn").click(function(e){
+            const tarefa = $("#tarefa").val();
+            if (tarefa==""){
+                alert("Digite uma tarefa válida");
+            }
+            else {
+                $("#lista").append(
+                    `
+                    <li>
+                        <i class="fas fa-check-circle check"></i>
+                        <span>${tarefa}</span>
+                        <i class="fas fa-trash-can exclude"></i>
+                    </li>
+                    `
+                );
+                $("#tarefa").val("");
+            }
+        
+            $(".exclude").each(function(){
+                $(this).click(function(){
+                    $(this).parent().remove();
+                })
+            });
+        
+            $("#lista li").click(function(){
+                $(this).find(".check").css("color","#349223");
+                $(this).find("span").css("textDecoration","line-through");
+            })
+        
+        })
+        
+
+    }
+
+
+)
+
+
+
+
+
+
+/* CÓDIGO SEM JQUERY
+
 const tarefa=document.querySelector("#tarefa");
 const btn=document.querySelector("#btn");
 const lista=document.querySelector("#lista")
@@ -14,16 +60,16 @@ btn.addEventListener("click",function(){
             <li>
                 <i class="fas fa-check-circle check"></i>
                 <span>${tarefa.value}</span>
-                <i class="fas fa-trash-can close"></i>
+                <i class="fas fa-trash-can exclude"></i>
             </li>
             `
     }
     tarefa.value=""; //Apagar do campo após incluir
 
-    const close=document.querySelectorAll(".close");    
-    for (let i=0;i<close.length;i++){
-            close[i].addEventListener("click", function(){
-                close[i].parentElement.remove();
+    const exclude=document.querySelectorAll(".exclude");    
+    for (let i=0;i<exclude.length;i++){
+            exclude[i].addEventListener("click", function(){
+                exclude[i].parentElement.remove();
             })
     }
 
@@ -32,3 +78,5 @@ btn.addEventListener("click",function(){
         e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
     })
 })
+    */
+
